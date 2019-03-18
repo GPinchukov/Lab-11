e55c3f5789444c7352dfdf45888eefdf132b772e
GPinchukov <Pinchukov000@gmail.com>
2019-03-18 21:48:40 +0300

    text

--- programm/main.c
+++ programm/main.c
@@ -1,47 +1,91 @@
-#include<stdio.h>
-main()
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main()
 {
-int i,j,k,n;
-float A[10][10],c,x[10],sum=0.0;
-printf("\n Vvedite razmernost matici ");
-scanf("%d",&n);
-printf("\n vedite elementy rashirenoy matrici \n\n" ); 
-for(i=1; i<=n; i++)
-{
-  for(j=1; j<=(n+1); j++)
+  double maxd, maxdp = 0, *B, *X, *d, A, ESP;
+  int i, j, n;
+  printf("Enter size of array: ");
+  scanf("%d", &n);
+
+  B = (double*)malloc(n * sizeof(double));
+  X = (double*)malloc(n * sizeof(double));
+  d = (double*)malloc(n * sizeof(double));
+  A = (double)malloc(n * sizeof(double*));
+  for (i = 0; i < n; ++i)
   {
-    printf("A[%d][%d] : ", i,j); 
-    scant("%f",&A[i][j]);
+    B[i] = 0; X[i] = 0; d[i] = 0;
+    A[i] = (double*)malloc(n * sizeof(double));
+    for (j = 0; j < n; ++j)
+    {
+      A[i][j] = 0;
+    }
   }
-}
-for(j=1; j<=n; j++)
-   {
-       for(i=1; i<=n; i++)
-       {
-        if(i>j)
+
+  for (i = 0; i < n; ++i)
+  {
+    for (j = 0; j < n; ++j)
+    {
+      printf("A[%d][%d]=", i, j);
+      scanf("%lf", &A[i][j]);
+    }
+    printf("\n");
+  }
+
+  for (i = 0; i < n; ++i)
+  {
+    printf("B[%d]=", i);
+    scanf("%lf", &B[i]);
+  }
+
+  printf("\nEnter measurement accurecy: ");
+  scanf_s("%lf", &ESP);
+
+  do
+  {
+    for (i = 0; i < n; ++i)
+    {
+      d[i] = X[i];
+      X[i] = B[i];
+      for (j = 0; j < n; ++j)
+      {
+        if (i != j)
         {
-           c=A[i][j]/A[j][j];
-            for(k=1; k<=n+1; k++)
-            {
-              A[i][k]=A[i][k]-c*A[j][k];
-            }
+          X[i] -= A[i][j] * X[j];
         }
-       }
-   }
-x[n]=A[n][n+1]/A[n][n];
-for(i=n-1; i>=1; i--)
+      }
+      X[i] = X[i] / A[i][i];
+      d[i] = fabs(d[i] - X[i]);
+    }
+    maxd = d[0];
+    for (i = 1; i < n; ++i)
+    {
+      if (d[i] > maxd) maxd = d[i];
+    }
+    if (maxdp == 0)
+    {
+      maxdp = maxd;
+      continue;
+    }
+    else
+    {
+      if (maxdp - maxd < 0)
+      {
+        break;
+      }
+      else
+      {
+        maxdp = maxd;
+      }
+    }
+  } while (maxd > ESP);
+
+  for (i = 0; i < n; ++i)
   {
-  sum=0;
-  for(j=i+1; j<=n; j++)
-       {
-         sum=sum+A[i][j]*x[j];
-       }
-    x[i]=(A[i][n+1]-sum)/A[i][i];
+    printf("X[%d]=%lf ", i, X[i]);
   }
-printf("\nResheniye: \n");
-   for(i=1; i<=n; i++)
-  {
-   printf("\nx%d=%f\t",i,x[i]); 
-  }
-retum(0);
+  system("pause");
 }
